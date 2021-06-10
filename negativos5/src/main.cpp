556689fd09f1cb61af137f1b2e2f0564fb66b1c2
silviafds <silviafernandes10@outlok.com.br>
2021-06-10 15:54:02 -0300

    atividade concluida

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -4,9 +4,19 @@ using std::cout;
 
 const int SIZE = 5; // input size.
 
-int main(void)
-{
-    // TODO: Adicione aqui seu código.
+int main(void){
+    
+    int cont = 0, vetor[5], a;
 
+    for(cont = 0; cont<SIZE; cont++){
+        cin >> vetor[cont];
+    }
+
+    for(cont = 0; cont<5; cont++){
+        if (vetor[cont] < 0)
+        a = a + 1;
+    }
+
+    cout<<"\n "<<a<<"\n";
     return 0;
 }
