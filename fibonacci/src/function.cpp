6fe165a833cacf071198fa0f972f91b98d6fb614
silviafds <silviafernandes10@outlok.com.br>
2021-06-14 16:43:43 -0300

    atividade realizada

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,33 @@
 #include "function.h"
+#include <iostream>
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
-{
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+std::vector<unsigned int> fib_below_n( unsigned int n ){
+    int t1 = 0, t2 = 1, t3 = 1;
+    std::vector<unsigned int> v;
+    while (t3 < n){
+        v.push_back(t3);
+        t3 = t1 + t2;
+        t1 = t2;
+        t2 = t3;
+    }
+    
+    return v;
 }
+
+/*int main(){
+    int tam, n;
+
+    cout << "Digite um número inteiro: ";
+    cin >> n;
+
+    std::vector<unsigned int> v = fib_below_n(n);
+
+    tam = v.size();
+    cout << "{ ";
+    for(int i=0; i<tam; i++){
+        cout << v[i] << "\t" << endl;
+    }
+    cout << " }";
+
+    return 0;
+}*/
--- fibonacci/src/function.h
+++ fibonacci/src/function.h
@@ -1,9 +1,13 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+#include <iostream>
+using namespace std;
+
 #include <vector>
 using std::vector;
 
 std::vector<unsigned int> fib_below_n( unsigned int n );
+//int main();
 
 #endif
